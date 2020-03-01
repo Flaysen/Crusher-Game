af38baf9a428be593e72945fc5284c4f7f127671
Flaysen <kamiltuszkiewicz@gmail.com>
2020-03-01 01:53:54 +0100

    Transform randomization in spawner class

--- Assets/Scripts/ScoreSystem/FrogSpawner.cs
+++ Assets/Scripts/ScoreSystem/FrogSpawner.cs
@@ -8,9 +8,19 @@ public class FrogSpawner : MonoBehaviour
 
     [SerializeField] private float _respawnRate;
 
+    [SerializeField] private float _sizeVariation;
+
     [SerializeField] private Vector3 _spawnPosition;
 
+    private Vector3 _frogInitialScale;
+
     private float _nextSpawnTime;
+
+    private void Start()
+    {
+        _frogInitialScale = FrogsPool.Instance.Get(Frog.FrogType.green).
+            GetComponent<Transform>().transform.localScale;
+    }
 
     private void Update()
     {
@@ -27,7 +37,9 @@ public class FrogSpawner : MonoBehaviour
         if(frog)
         {
             frog.transform.position = CalculatePosition();
-            frog.transform.rotation = new Quaternion(0, 90, 0, 0);
+            frog.transform.rotation = new Quaternion(0,0,0,0);
+            frog.transform.Rotate(Vector3.up, Random.Range(0, 360));
+            frog.transform.localScale = _frogInitialScale * ( 1 + _sizeVariation);
             frog.gameObject.SetActive(true);
             frog.Initialize(_scoreManager);
         }
@@ -35,13 +47,10 @@ public class FrogSpawner : MonoBehaviour
     }
     private Vector3 CalculatePosition()
     {
-        float x;
-        float z;
-
         int a = Random.Range(0, 2);
         _spawnPosition = a == 0 ? new
-            Vector3(Random.Range(0, 4) * 6 - 9, 2.5f, Random.Range(-13.0f, 13.0f)) :
-             new Vector3(Random.Range(-13.0f, 13.0f), 2.5f, Random.Range(0, 4) * 6 - 9);
+            Vector3(Random.Range(0, 4) * 6 - 9, 2.0f, Random.Range(-13.0f, 13.0f)) :
+             new Vector3(Random.Range(-13.0f, 13.0f), 2.0f, Random.Range(0, 4) * 6 - 9);
 
         return _spawnPosition;
     }
